6273a697a17227f748984dda7140684026f3ffbe
Alessandra.O <alessandra.orban@yahoo.com>
2025-01-31 20:10:38 +0200

    Implementare aplicație de notițe - Orban Alessandra

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MentorNotes from "./pages/mentor/AddNotes";
+import AddNotes from "./pages/orban_alessandra/AddNotes"; // Import componenta ta
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -10,10 +11,11 @@ function App() {
         <h1>Note App</h1>
         <Routes>
           <Route path='/' element={<MentorNotes />} />
-          {/* Add your route here */}
+          <Route path='/orban-alessandra' element={<AddNotes />} /> {/* Ruta pentru componenta ta */}
         </Routes>
       </div>
     </Router>
   );
 }
+
 export default App;
